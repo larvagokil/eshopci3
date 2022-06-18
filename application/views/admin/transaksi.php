aa5c5f1c05b33e503f76b120b872a42f42ce6d26
adhit007 <muhammad.adhitya007@gmail.com>
2022-06-19 00:48:48 +0700

    membuat views menu admin (menu data barang, edit profile, my profile, data transaksi, halaman akses blokir)

---
+++ application/views/admin/transaksi.php
@@ -0,0 +1,133 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Barang yang dipesan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Earnings (Annual) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Barang yang diantarkan</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Tasks Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Barang yang diproses
+                            </div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">2</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Barang yang belum dibayar</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-file-invoice-dollar fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">id barang</th>
+                            <th scope="col">id transaksi</th>
+                            <th scope="col">Nama Lengkap</th>
+                            <th scope="col">No Telp</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Jumlah barang</th>
+                            <th scope="col">total harga</th>
+                            <th scope="col">Jenis Kirim</th>
+                            <th scope="col">Jenis Bayar</th>
+                            <th scope="col">status</th>
+                            <th scope="col">waktu transaksi</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $i = 1; ?>
+                        <?php foreach($trans as $trs) : ?>   
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td><?= $trs['id_barang']; ?></td>
+                            <td><?= $trs['id_transaksi']; ?></td>
+                            <td><?= $trs['nm_lengkap']; ?></td>
+                            <td><?= $trs['no_telp']; ?></td>
+                            <td><?= $trs['alamat']; ?></td>
+                            <td><?= $trs['jml_barang']; ?></td>
+                            <td><?= $trs['total_harga']; ?></td>
+                            <td><?= $trs['jeniskirim']; ?></td>
+                            <td><?= $trs['jenisbayar']; ?></td>
+                            <td><?= $trs['status']; ?></td>
+                            <td><?= $trs['waktu_transaksi']; ?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+</div>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
