b924c59475fac94db7a3fe97b23173e0e99cce4c
adhit007 <muhammad.adhitya007@gmail.com>
2022-06-21 18:45:39 +0700

    menambahkan model adminm pada autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('barangm');
+$autoload['model'] = array('barangm', 'adminm');
