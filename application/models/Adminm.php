bcfd0c84b5ec5db47e8074b8dc35bca421979418
adhit007 <muhammad.adhitya007@gmail.com>
2022-06-22 00:39:53 +0700

    memperbaiki code pada model Adminm fungsi edit

--- application/models/Adminm.php
+++ application/models/Adminm.php
@@ -17,4 +17,29 @@ class Adminm extends CI_Model
     {
         return $this->db->get('transaksi');
     }
+
+    public function edit($name, $email, $new_image)
+    {
+        $this->db->set('name', $name);
+        $this->db->where('email', $email);
+        $this->db->update('users', $new_image);
+    }
+
+    public function upload($upload_image)
+    {
+        if ($upload_image) {
+            $config['allowed_types'] = 'svg|jpg|png';
+            $config['max_size']     = '2048';
+            $config['upload_path'] = './assets/img/profile/';
+
+            $this->load->library('upload', $config);
+
+            if ($this->upload->do_upload('image')) {
+                $new_image = $this->upload->data('file_name');
+                $this->db->set('image', $new_image);
+            } else {
+                echo $this->upload->display_errors();
+            }
+        }
+    }
 }
