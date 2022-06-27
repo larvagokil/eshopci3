fc8a6c4a62ee576abdca251b55cef9f8dfee7e83
adhit007 <muhammad.adhitya007@gmail.com>
2022-06-28 00:17:43 +0700

    menambahkan script pada footer, utk preview img user

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -68,6 +68,22 @@
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  <script>
+      function previewImg() {
+          const image = document.querySelector('#image');
+          const imageLabel = document.querySelector('.form-control');
+          const imgPreview = document.querySelector('.img-thumbnail');
+
+          imageLabel.textContent = image.files[0].name;
+          const fileImage = new FileReader();
+          fileImage.readAsDataURL(image.files[0]);
+
+          fileImage.onload = function(e) {
+              imgPreview.src = e.target.result;
+          }
+      }
+  </script>
   </body>
 
   </html>
