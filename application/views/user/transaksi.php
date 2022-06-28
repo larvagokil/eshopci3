aa3bd78feac7079edef421d156b2eec4a6222a0c
larvagokil <akhmadghazi@yahoo.com>
2022-06-28 14:19:06 +0700

    add logic jika belum ada transaksi

--- application/views/user/transaksi.php
+++ application/views/user/transaksi.php
@@ -3,6 +3,16 @@
     <?= $this->session->flashdata('pesan'); ?>
     <div class="row">
         <?php
+        $cek = $this->db->get_where('transaksi', ['nm_user' => $this->session->userdata('email')])->num_rows();
+            if ($cek < 1) {
+        ?>
+        <div class="col mb-4 pt-3">
+        <blockquote class="blockquote text-center">
+            <p class="mb-0 font-italic">Belum ada Riwayat transaksi, silahkan berbelanja terlebih dahulu</p>
+        </blockquote> 
+        </div>
+        <?php
+        } else {
            foreach ($trx as $tr) {
         ?>
         <div class="col-md-4 mb-4 pt-3">
@@ -23,23 +33,23 @@
             </div>    
         </div>
         <?php
-           }
+           } }
         ?>
     </div>
     <p><small>
         Note : 
         <ul>
             <li>
-                <span class="badge bg-info">Menunggu Konfirmasi</span> : Menunggu Admin untuk memproses Transaksi.
+                <span class="badge bg-info text-light">Menunggu Konfirmasi</span> : Menunggu Admin untuk memproses Transaksi.
             </li>
             <li>
-                <span class="badge bg-primary">Diproses</span> : Transaksi telah di Konfirmasi & Barang akan dikirim ke alamat yang telah ditentukan sebelumnya.
+                <span class="badge bg-primary text-light">Diproses</span> : Transaksi telah di Konfirmasi & Barang akan dikirim ke alamat yang telah ditentukan sebelumnya.
             </li>
             <li>
-                <span class="badge bg-success">Selesai</span> : Transaksi Telah selesai.
+                <span class="badge bg-success text-light">Selesai</span> : Transaksi Telah selesai.
             </li>
             <li>
-                <span class="badge bg-danger">Gagal</span> : Transaksi telah di Tolak, Karena (Alamat tidak benar / No telepon tidak bisa dihubungi / lainnya)
+                <span class="badge bg-danger text-light">Gagal</span> : Transaksi telah di Tolak, Karena (Alamat tidak benar / No telepon tidak bisa dihubungi / lainnya)
             </li>
         </ul>
     </small></p>
